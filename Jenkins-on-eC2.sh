881609f1ea3c9ade68259b7dd42a7ce6435d267a
debadattagadanayak <gadanayakofficial@gmail.com>
2023-04-20 16:58:45 +0530

    new

--- Jenkins-on-eC2.sh
+++ Jenkins-on-eC2.sh
@@ -20,14 +20,22 @@ sudo amazon-linux-extras install java-openjdk11 -y
 # Install Jenkins
 sudo yum install jenkins -y
 
+sleep 10s
+
 # Enable jenkins too run any command without asking password
 echo 'jenkins ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers
+
+sleep 10s
 
 # Enable Jenkins service to start at boot
 sudo systemctl enable jenkins
 
+sleep 10s
+
 # Start Jenkins as a service
 sudo systemctl start jenkins
+
+sleep 10s
 
 # Check the status of the Jenkins service
 sudo systemctl status jenkins
